43556d863f9f50e699828a42be85bf303377b013
Pedro Marcondes <pedro.marcondes12@hotmail.com>
2024-11-12 22:18:17 -0300

    Alterar o número máximo para 50.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaNumerosSorteados = [];
-let numeroMaximo = 20, numeroSecreto = gerarNumeroAleatorio(numeroMaximo);
+let numeroMaximo = 50, numeroSecreto = gerarNumeroAleatorio(numeroMaximo);
 let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do Número Secreto');
-    exibirTextoNaTela('p', 'Dê um chute para adivinhar o número.');
+    exibirTextoNaTela('p', 'Dê um chute para adivinhar o número entre 1 e 50.');
 }
 
 exibirMensagemInicial();
